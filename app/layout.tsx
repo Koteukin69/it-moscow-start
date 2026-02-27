f69df5f8e522795c4471b3b625933109ebc45296
Koteukin <44315626+Koteukin69@users.noreply.github.com>
2026-02-27 23:23:13 +0300

    Delete wrong code

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,19 +21,6 @@ export default function RootLayout({
   return (
     <html lang="ru" suppressHydrationWarning>
       <head>
-        <!-- Yandex.Metrika counter -->
-        <script type="text/javascript">
-          (function(m,e,t,r,i,k,a){
-          m[i]=m[i]function(){(m[i].a=m[i].a[]).push(arguments)};
-          m[i].l=1*new Date();
-          for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
-          k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
-        })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=107044008', 'ym');
-
-          ym(107044008, 'init', {ssr:true, webvisor:true, clickmap:true, ecommerce:"dataLayer", referrer: document.referrer, url: location.href, accurateTrackBounce:true, trackLinks:true});
-        </script>
-        <noscript><div><img src="https://mc.yandex.ru/watch/107044008" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
-        <!-- /Yandex.Metrika counter -->
       </head>
       <body
         className={`${montserrat.variable} antialiased`}

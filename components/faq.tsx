7be231b810d2f489f624bae692b4fb1cc2aee7ef
Koteukin <44315626+Koteukin69@users.noreply.github.com>
2026-03-02 00:48:00 +0300

    Smooth scroll and faq page text animation.

--- components/faq.tsx
+++ components/faq.tsx
@@ -5,18 +5,33 @@ import {Button} from "@/components/ui/button";
 import {useState} from "react";
 
 export default function FAQ({questions}: {questions: {question: string, answer: string}[]}) {
-  const [anwser, setAnwser] = useState<string>("Выбери вопрос — я отвечу!");
+  const [answer, setAnswer] = useState<string>("Выбери вопрос — я отвечу!");
+  const [intervalValue, setIntervalValue] = useState<NodeJS.Timeout>();
 
   return <div className={"flex flex-col md:flex-row justify-center min-h-dvh items-center"}>
     <div className={"flex w-full flex-col justify-center items-center"}>
       <div className={"w-full max-w-100 aspect-square"}>
         <Orb/>
       </div>
-      <div className={"text-center min-h-30 max-w-sm font-semibold"}>{anwser}</div>
+      <div className={"text-center min-h-30 max-w-sm font-semibold"}>{answer}</div>
     </div>
-    <div className={"flex max-w-sm sm:max-w-md flex-col justify-center gap-3 pr-10 md:pr-20 h-fit pb-20"}>
+    <div className={"flex max-w-sm sm:max-w-md flex-col justify-center gap-3 pr-10 md:pr-20 h-fit py-20"}>
       {questions.map((question, index) => (
-        <Button variant={"link"} className={"text-wrap h-fit w-fit text-left"} key={index} onClick={() => {setAnwser(question.answer)}}>{question.question}</Button>
+        <Button variant={"link"} className={"text-wrap h-fit w-fit text-left"} key={index} onClick={() => {
+          window.scrollTo(0, 0);
+          let i = 0;
+          let result = "";
+
+          const interval = setInterval(() => {
+            result+=question.answer[i];
+            setAnswer(result);
+            if (i === question.answer.length - 1) clearInterval(interval);
+            i++;
+          }, 10);
+
+          clearInterval(intervalValue);
+          setIntervalValue(interval);
+        }}>{question.question}</Button>
       ))}
     </div>
   </div>;

d790591ba7030b7040002e039185324cbd448186
Koteukin <petr.s.chernov@gmail.com>
2026-05-11 23:22:26 +0300

    Create /apt/health route for server health statuschecking

---
+++ app/api/health/route.ts
@@ -0,0 +1,10 @@
+export const dynamic = "force-dynamic";
+import {NextResponse} from "next/server";
+
+export async function GET() {
+    return NextResponse.json({
+        ok: true,
+        status: "healthy",
+        time: new Date().toISOString(),
+    });
+}
